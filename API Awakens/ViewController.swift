6742a2268dfdb789bd01ff81aa0b6fdb82b5a8e1
tayalva <tay.smith@me.com>
2017-07-13 10:42:12 -0700

    won't catch data

--- API Awakens/ViewController.swift
+++ API Awakens/ViewController.swift
@@ -13,12 +13,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let fetchData = NetworkManager()
+        
+       
+        fetchData.fetchData { (fetchedInfo) in
+            
+            print(fetchedInfo)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
 
 
 }
